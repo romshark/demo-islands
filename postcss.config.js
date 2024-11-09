3da75bcab17c4bd11a6cff212c602ccad291ad35
Roman Sharkov <roman.scharkov@gmail.com>
2024-11-09 10:30:49 +0100

    perf: Minify dist.css

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,16 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
 
 export default {
   content: [
     "./server/**/*.templ", // Include any .templ files
   ],
-  plugins: [tailwindcss, autoprefixer],
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    cssnano({
+      preset: "default",
+    }),
+  ],
 };
