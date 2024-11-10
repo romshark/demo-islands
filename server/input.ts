c712de6390c136f5f3490ef6b2ad925acb90313d
Roman Sharkov <roman.scharkov@gmail.com>
2024-11-11 00:40:00 +0100

    feat: Themes

--- server/input.ts
+++ server/input.ts
@@ -1,11 +1,22 @@
 import htmx from "htmx.org";
+import Alpine from "alpinejs";
 
 window.htmx = htmx;
 
 declare global {
   interface Window {
     htmx: typeof htmx;
+    app: App;
   }
+}
+
+interface App {
+  testValue: string;
+  theme: Theme | null;
+  themeLinkEl: HTMLLinkElement | null;
+  init(): void;
+  setTheme(theme: Theme): void;
+  applyTheme(): void;
 }
 
 // hxThresholdLoadingIndicator defines how long to wait before
@@ -44,3 +55,112 @@ function slOptionsReveal() {
 // parts of the page are reloaded to avoid CLS (cumulative layout shift).
 window.addEventListener("load", slOptionsReveal);
 document.addEventListener("htmx:afterSwap", slOptionsReveal);
+
+enum Theme {
+  Light = "light",
+  Dark = "dark",
+}
+
+const themes = {
+  [Theme.Light]: {
+    className: "sl-theme-light",
+    url: "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.18.0/cdn/themes/light.css",
+  },
+  [Theme.Dark]: {
+    className: "sl-theme-dark",
+    url: "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.18.0/cdn/themes/dark.css",
+  },
+};
+
+function getSystemTheme(): Theme {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return Theme.Dark;
+  }
+  return Theme.Light;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("alpine:init", () => {
+    Alpine.data("app", () => ({
+      testValue: "dark",
+      theme: null as Theme | null,
+      themeLinkEl: null as HTMLLinkElement | null,
+
+      init() {
+        const themeSetting = localStorage.getItem("theme");
+        switch (themeSetting) {
+          case "light":
+            this.$refs.themeSelect.setAttribute("value", "light");
+            this.theme = Theme.Light;
+            break;
+          case "dark":
+            this.$refs.themeSelect.setAttribute("value", "dark");
+            this.theme = Theme.Dark;
+            break;
+          default:
+            this.$refs.themeSelect.setAttribute("value", "");
+            break;
+        }
+        this.applyTheme();
+
+        // Watch for changes to the system preference.
+        window
+          .matchMedia("(prefers-color-scheme: dark)")
+          .addEventListener("change", () => {
+            if (this.theme == null) {
+              this.applyTheme();
+            }
+          });
+
+        // Expose the app component to the window API.
+        window.app = this;
+      },
+
+      setTheme(theme: Theme) {
+        if (Object.values(Theme).includes(theme)) {
+          localStorage.setItem("theme", theme);
+          this.theme = theme;
+        } else {
+          localStorage.removeItem("theme");
+          this.theme = null;
+        }
+        this.applyTheme();
+      },
+
+      applyTheme() {
+        const current = this.theme ?? getSystemTheme();
+
+        // Create and insert a new link element for the theme.
+        const newEl = document.createElement("link");
+        newEl.setAttribute("rel", "stylesheet");
+        newEl.setAttribute("href", themes[current].url);
+        document.head.appendChild(newEl);
+
+        newEl.addEventListener(
+          "load",
+          () => {
+            // Switch styles.
+            document.body.classList.remove(
+              themes[Theme.Light].className,
+              themes[Theme.Dark].className
+            );
+            document.body.classList.add(themes[current].className);
+            this.themeLinkEl?.remove();
+            this.themeLinkEl = newEl;
+          },
+          { once: true }
+        );
+
+        newEl.addEventListener(
+          "error",
+          () => {
+            console.error(`failed loading theme ${current}`);
+          },
+          { once: true }
+        );
+      },
+    }));
+  });
+
+  Alpine.start();
+});
